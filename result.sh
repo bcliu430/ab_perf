1a8b90cf98a66e0dbad5de85498d1a8439841de3
root <lbc0430@gmail.com>
2017-01-13 00:19:57 -0500

    add data

--- result.sh
+++ result.sh
@@ -2,12 +2,22 @@
 
 set -euo pipefail
 
+source pv-deploy-config.mk
 
-ID=0
+POSTFIX=".dat"
+TEMP="temp"
+
+echo -n "" > temp.dat;
 echo -n "" > result.csv
-for i in `seq $ID $(($1-1))`; do
-	cat vm$i.dat | grep "Request" | cut -d " " -f  7 >> "result.csv"
+for i in `seq  1 7`; do
+	cat vm$i.dat | grep "Request" | cut -d " " -f  7 >> "temp.dat"
 done
 
-echo "please check the result in result.csv"
+ cat temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq /     NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print  sum / NR " ," sqrt(sqd) }' >> result.csv
 
+
+mkdir data 
+
+mv *.dat *.csv ./data
+
+echo "please check the result in result.csv"
