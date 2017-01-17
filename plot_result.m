6d94dd53907efa54d242883e013f3485d26a7af2
root <lbc0430@gmail.com>
2017-01-17 16:21:31 -0500

    add plot

--- plot_result.m
+++ plot_result.m
@@ -3,8 +3,10 @@ clear
 Array=csvread('result.csv');
 VCPU = Array(:,1);
 ReqPerSec = Array(:,2);
+Stdev = Array(:,3);
 
 figure
-plot(VCPU,ReqPerSec)
+plot(VCPU,ReqPerSec,VCPU, Stdev)
+legend('ReqPerSec','Stdev')
 saveas(gcf,'ab_perf.jpg')
 
